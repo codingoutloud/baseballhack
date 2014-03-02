03665c9e25eb73d3dd5f2be75c9ec6c20e76e80d
Maura Wilder <maura@devpartners.com>
2014-03-02 16:11:30 -0500

    get url of deep-zoomed image, pass image name to script

--- python/hitzoomit.py
+++ python/hitzoomit.py
@@ -1,11 +1,15 @@
 import mechanize
 import urllib
 import urllib2
+import sys
+
+imageurl = sys.argv[1]
 
 browser = mechanize.Browser()
+browser.set_handle_redirect(False)
 response = browser.open("http://zoom.it/")
 
-values = {'url' : 'https://avatars.githubusercontent.com/u/560657',
+values = {'url' : imageurl,
           'x' : '35',
           'y' : '8'
           }
@@ -13,6 +17,5 @@ data = urllib.urlencode(values)
 req = urllib2.Request("http://zoom.it/", data)
 response2 = urllib2.urlopen(req)
 
-print response.geturl()
-print response.info()
-print response.read()
+# This is the short url of the deepzoom image
+print response2.geturl()
